1db3d1baf32e067457e86fc67a00ed026751f146
iampassos <78429887+iampassos@users.noreply.github.com>
2025-03-29 01:46:00 -0300

    Moved lib to main

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,3 @@
-use std::{
-    fs,
-    io::{Result, prelude::*},
-};
-
-pub fn run(source: &str) {
-    print!("{source}");
-}
-
-pub fn run_script(path: &String) -> Result<()> {
-    let script = fs::read_to_string(path)?;
-    run(script.as_str());
-    Ok(())
-}
-
-pub fn run_prompt() -> Result<()> {
-    loop {
-        print!("> ");
-
-        let mut input = String::new();
-        std::io::stdout().flush()?;
-        std::io::stdin().read_line(&mut input)?;
-
-        if input.trim() == "exit" {
-            break;
-        }
-
-        run(input.as_str());
-    }
-
-    Ok(())
-}
-
-pub fn error(line: u32, message: &str) {
+pub fn error(line: usize, message: String) {
     println!("Error in line {line}:\n {message}");
 }
